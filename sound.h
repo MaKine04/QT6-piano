ed10b55e56578297fe34ff050d4cd0a06a6539e5
MaKine04 <165089190+MaKine04@users.noreply.github.com>
2025-12-05 12:27:28 +0100

    Add sound playback function in sound.h

---
+++ sound.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <QSoundEffect>
+#include <QObject>
+#include <QDebug>
+
+void playsound(QString &track, QObject *parent = nullptr)
+{
+    QSoundEffect *clickSound = new QSoundEffect;
+    QString filepath = "/home/martinka/Work/piano/keysound/" + track;
+    clickSound -> setSource(QUrl::fromLocalFile(filepath));
+    clickSound->setVolume(1.0f);
+    clickSound->play();
+
+}
