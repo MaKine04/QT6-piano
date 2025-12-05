dfca8b3f02a06e6f2333cb44d87db8b48f8172be
MaKine04 <165089190+MaKine04@users.noreply.github.com>
2025-12-05 17:31:53 +0100

    Update sound file path for playsound function

--- sound.h
+++ sound.h
@@ -7,7 +7,7 @@
 void playsound(QString &track, QObject *parent = nullptr)
 {
     QSoundEffect *clickSound = new QSoundEffect;
-    QString filepath = "/home/martinka/Work/piano/keysound/" + track;
+    QString filepath = "/home/piano/keysound/" + track;
     clickSound -> setSource(QUrl::fromLocalFile(filepath));
     clickSound->setVolume(1.0f);
     clickSound->play();
