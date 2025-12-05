af1acd04d501259caa86331477ca784bb77bf4ea
MaKine04 <165089190+MaKine04@users.noreply.github.com>
2025-12-05 17:36:11 +0100

    Update sound.h

--- sound.h
+++ sound.h
@@ -7,7 +7,7 @@
 void playsound(QString &track, QObject *parent = nullptr)
 {
     QSoundEffect *clickSound = new QSoundEffect;
-    QString filepath = "/home/piano/keysound/" + track;
+    QString filepath = "/piano/keysound/" + track;
     clickSound->setSource(QUrl::fromLocalFile(filepath));
     clickSound->setVolume(1.0f);
     clickSound->play();
