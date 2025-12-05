1e9e348ae345a661b26d79b5e46b86d55bfd9a24
MaKine04 <165089190+MaKine04@users.noreply.github.com>
2025-12-05 17:32:10 +0100

    Fix typo in playsound function for setting source

--- sound.h
+++ sound.h
@@ -8,7 +8,7 @@ void playsound(QString &track, QObject *parent = nullptr)
 {
     QSoundEffect *clickSound = new QSoundEffect;
     QString filepath = "/home/piano/keysound/" + track;
-    clickSound -> setSource(QUrl::fromLocalFile(filepath));
+    clickSound->setSource(QUrl::fromLocalFile(filepath));
     clickSound->setVolume(1.0f);
     clickSound->play();
 
